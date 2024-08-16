8cbb41d9f0d0477b00f4ca982333bbf23aadcba5
Andreygolpy <anywhatkv@gmail.com>
2024-08-16 12:21:17 +0300

    comit init

---
+++ par_bank.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import time
+
+# URL для отзывов
+url = "https://www.banki.ru/services/responses/list"
+
+
+respons = requests.get(url)
+
